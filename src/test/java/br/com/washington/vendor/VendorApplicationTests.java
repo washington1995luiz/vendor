ae34b62268bdab07b11651d315a03800afa1d92d
Washington <washington195luiz@gmail.com>
2025-02-23 17:53:02 -0300

    First commit

---
+++ src/test/java/br/com/washington/vendor/VendorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.washington.vendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
